94ff6d4d8c26d0b4c7ed664179bd4492670aa141
Richmond Anku <R.s.a.anku@tudelft.nl>
2024-03-16 14:35:10 +0100

    still setting up repository

--- src/hydrodynamics/inputs.py
+++ src/hydrodynamics/inputs.py
@@ -2,33 +2,33 @@ import math
 import pandas
 import numpy
 import sys
-import data.py
+import data
+
 
 class INPUT:
     type = "None"
-    rho = 1.00,  # density of water [kg/m^3]
-    g = 9.81,  # Acceleration due to gravity [m/s^2]
+    rho = (1.00,)  # density of water [kg/m^3]
+    g = (9.81,)  # Acceleration due to gravity [m/s^2]
+
     def __init__(self, data1=None, data2=None, data3=None) -> None:
         self.vessel_data = data1
         self.fairway_data = data2
         self.prop_data = data3
- 
-    def hydrostatics():
-        disp = C_b * (self.vessel_data.get("L_oa")*vessel_data.get("B")*vessel_data.get("T"))*
-        vol_disp =
-        C_b =
-        C_m =
-        C_p =
-        LCB =
-        WSA = 
 
-    def der_par():
-        Re = V*L/nu
-        Fr_h = V/sqrt(g*L)
+    # def hydrostatics():
+    #     disp = C_b * (self.vessel_data.get("L_oa")*vessel_data.get("B")*vessel_data.get("T"))*
+    #     vol_disp =
+    #     C_b =
+    #     C_m =
+    #     C_p =
+    #     LCB =
+    #     WSA =
 
-
+    # def der_par():
+    #     Re = V*L/nu
+    #     Fr_h = V/sqrt(g*L)
 
 
-data = INPUT(data1=vessel_data)
+# data = INPUT(data1=vessel_data)
 
 print(data.vessel_data.get("L_oa"))
--- src/hydrodynamics/resistance.py
+++
@@ -1,5 +0,0 @@
-import math
-import pandas
-import numpy
-import sys
-from pyomo.environ import *
